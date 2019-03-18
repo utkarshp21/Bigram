bf69d9426d380b8c58fba0c4a3676e306706fecc
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-03-17 20:14:16 -0400

    removed unused code

--- src/main/java/bigram/BigramCount.java
+++ src/main/java/bigram/BigramCount.java
@@ -1,23 +1,18 @@
 package bigram;
-import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-//import org.apache.hadoop.mapreduce.Mapper;
-//import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//import org.apache.hadoop.util.GenericOptionsParser;
 
 public class BigramCount {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
 
-        Job job = new Job(conf, "word count");
+        Job job = new Job(conf, "Biagram");
         job.setJarByClass(BigramCount.class);
 
         job.setMapperClass(BigramMapper.class);
--- src/main/java/bigram/BigramMapper.java
+++ src/main/java/bigram/BigramMapper.java
@@ -12,11 +12,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class BigramMapper
-        extends Mapper<Object, Text, Text, IntWritable>{
+public class BigramMapper extends Mapper<Object, Text, Text, IntWritable>{
 
     private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+   
     private Text Bigram = new Text();
 
     public void map(Object key, Text value, Context context
